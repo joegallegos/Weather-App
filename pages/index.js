9a667f2c4d5bcd4a57bb7185e8ed36861b91def1
Joe Gallegos <j.gallegos25u@gmail.com>
2020-10-29 08:51:48 -0500

    change background on card

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home({
           <h1 className={clsx('text-5xl mb-8', cloud <= 30 ? 'text-green-600' : 'text-red-600')}>
             {cloud > 30 ? 'Not worth it' : 'Go for it dude!'}
           </h1>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-200">
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">
                 <p>
@@ -71,7 +71,7 @@ export default function Home({
               </p>
             </div>
             <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              <span className="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                 Last Updated: <ConvertDate dateString={updated} />
               </span>
             </div>
