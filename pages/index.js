6ebe94cc84409c2546dbb74099c8b5ddb3fecb64
Joe Gallegos <j.gallegos25u@gmail.com>
2020-12-07 10:10:48 -0700

    fix base url

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home({}) {
     e.preventDefault();
     setZip('');
 
-    const baseUrl = 'https://api.weatherapi.com/v1';
+    const baseUrl = 'http://api.weatherapi.com/v1';
     const day = new Date();
     const today = day.getDate();
 
